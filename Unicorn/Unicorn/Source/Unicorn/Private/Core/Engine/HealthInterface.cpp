85c0fd98c44a9a14c6a978c88cf5d18267475ce6
Justin Camden <camden@usc.edu>
2018-01-22 15:20:23 -0800

    Added c++ health interface

---
+++ Unicorn/Unicorn/Source/Unicorn/Private/Core/Engine/HealthInterface.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HealthInterface.h"
+
+UHealthInterface::UHealthInterface(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+
+}
+
+
---
+++ Unicorn/Unicorn/Source/Unicorn/Public/Core/Engine/HealthInterface.h
@@ -0,0 +1,26 @@
+// Copyright 2018 Team Unicorn All Rights Reserved
+
+#pragma once
+
+#include "Object.h"
+#include "Interface.h"
+#include "HealthInterface.generated.h"
+
+/**
+ * 
+ */
+UINTERFACE(BlueprintType)
+class UNICORN_API UHealthInterface : public UInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+//Create a new class ILookable and use GENERATED_IINTERFACE_BODY() Makro in class body
+class UNICORN_API IHealthInterface
+{
+	GENERATED_IINTERFACE_BODY()
+
+public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Combat")
+	bool LoseHealth(float HealthLost);
+};
