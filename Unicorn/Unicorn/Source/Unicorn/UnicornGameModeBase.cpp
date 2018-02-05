24f178cf0379363d5589c7fd843e02f321994ba1
Justin Camden <camden@usc.edu>
2018-02-05 01:20:30 -0800

    Workng on more stuff

--- Unicorn/Unicorn/Source/Unicorn/UnicornGameModeBase.cpp
+++ Unicorn/Unicorn/Source/Unicorn/UnicornGameModeBase.cpp
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "UnicornGameModeBase.h"
 #include "UnicornAIManager.h"
 #include "Runtime/Engine/Classes/Engine/World.h"
-#include "UnicornGameModeBase.h"
+
 
 AUnicornGameModeBase::AUnicornGameModeBase(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
